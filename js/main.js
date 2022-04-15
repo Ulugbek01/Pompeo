11a5f7e336d78812131d8e3edde26579ac077de7
Ulugbek01 <xyz207820@gmail.com>
2022-04-15 21:23:57 +0500

    Header section add

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const navItems = document.querySelectorAll(".main-navigation__items a");
+navItems.forEach((value) => {
+  value.addEventListener("click", () => {
+    // value.classList.contains
+  });
+});
